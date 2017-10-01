84eb8d81b5dcd2465e8c70f1e4245d3bdb11c868
Deddy T <deddybuster@hotmail.com>
2017-10-01 20:27:01 +0800

    Update Project-age
    Added another way of doing it

--- Project2-age.py
+++ Project2-age.py
@@ -9,3 +9,12 @@ user_age = int(input('What is your age? \n'))
 print('Oh so your age is', user_age)
 years = int(input('Enter a number to know your age after that many years \n'))
 print('You will be',years+user_age, 'in', years ,'years')
+
+
+# Another way of doing
+user_age = int(input('Hello!\nWhat is your age? '))
+# Formatting will make the message looks better
+response = 'Oh so your age is {0}.'.format(user_age)
+print(response)
+years = int(input('Enter a number to know your age after that many years: '))
+print('You will be', years + user_age, 'in', years, 'years.')
