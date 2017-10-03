7aef4799ed4c4b44c932038ea4f4e8b80d50152f
mtoadvine <toad1225@gmail.com>
2017-10-02 21:20:29 -0400

    Reduced number of operations
    Reduced the number of operations within the code to two

--- average_of_3_numbers.py
+++ average_of_3_numbers.py
@@ -3,12 +3,11 @@ Author: Pranav Goel
 Takes three float numbers and returns their average
 """
 
-total = 0.0
-number1=float(input("Enter the first number: "))
-total = total + number1
-number2=float(input("Enter the second number: "))
-total = total + number2
-number3=float(input("Enter the third number: "))
-total = total + number3
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+num3=float(input("Enter the third number: "))
+
+total = num1 + num2 + num3
 average = total / 3
+
 print ("The average is " + str(average))
