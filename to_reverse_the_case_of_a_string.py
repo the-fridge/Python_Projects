edd3f35b9f0f4cdab8bd2c42be261b9e50df3168
pBouillon <pierre.bouillon@openmailbox.org>
2017-10-03 12:08:59 +0200

    renamed and formatted script

---
+++ to_reverse_the_case_of_a_string.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# authors: unknown / Pierre Bouillon
+
+"""`to_reverse_the_case_of_string.py` Takes a string an 
+print it with its case swapped
+"""
+
+if __name__ == '__main__':
+    print(
+        input('Enter a word to reverse the case of string: ')
+        .swapcase()
+    )
