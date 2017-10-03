c73a10711c4fcfb3fc6bd60e84b44aa8ac1b50c3
pBouillon <pierre.bouillon@openmailbox.org>
2017-10-03 11:19:52 +0200

    reworked infinity loop execution

--- Project2-annoying_while_loop.py
+++ Project2-annoying_while_loop.py
@@ -1,9 +1,14 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# authors: Syed Marwan Jamal / Pierre Bouillon
+
+"""`Project2-annoying_while_loop.py` is a program with
+an infinity loop
 """
-  A never ending while_loop which never terminates
-"""
-"""
-  While True:
-    print("Hello World")
-"""
-while True:
-    print('Hello World')
+
+if __name__ == '__main__':
+    try:
+        while True:
+            print('Hello World')
+    except KeyboardInterrupt:
+        print('Ending the program')
