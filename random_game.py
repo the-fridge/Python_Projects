a04f93f169c5be46b38ccc72855ee5cf0351a5ce
pBouillon <pierre.bouillon@openmailbox.org>
2017-10-03 15:06:49 +0200

    commented code

--- random_game.py
+++ random_game.py
@@ -9,15 +9,32 @@ CODE_LENGTH = 3
 MAX_DIGIT   = 10
 
 class Secret_Code(object):
+    """ Build and check a secret code
+    """
     def __init__(self):
         self._solved = False
         self._code = self._generate ()
 
     def _generate(self):
+        """ Generate the secret code
+
+        Pick CODE_LENGTH numbers between 0 and MAX_DIGIT
+
+        Returns:
+            - : (int[]) A 3 digits code 
+        """
         code = [num for num in range(MAX_DIGIT)]
         return sample(code, CODE_LENGTH)
 
     def _get_matches(self, usr_list):
+        """ Parse list and compare it with the secret code
+
+        Attribute:
+            - usr_list : (int[]) the user input
+
+        Returns:
+            - match : (int) number of digits in the right place
+        """
         match = 0
         for i in range(len(self._code)):
             if usr_list[i] == self._code[i]:
@@ -25,20 +42,38 @@ class Secret_Code(object):
         return match
 
     def guess(self, usr_code):
-        match    = 0
+        """ Compare the user code and the secret code
 
+        convert the user code as a list and compare it
+        pass its attribute _solved on true if everything match
+
+        Attributes:
+            - usr_code : (str) the input of the user
+
+        Returns:
+            - 'Well done' if everything match
+            - 'Almost ! {} correct(s)' if not everything match
+            - 'No match' if nothing match
+        """
+        match    = 0
         usr_code = [int(c) for c in usr_code]
+
         match = self._get_matches(usr_code)
 
         if match == len(self._code):
             self._solved = True
             return 'Well done !'
         elif match > 0 :
-            return 'Almost {} correct(s)'.format(match)
+            return 'Almost ! {} correct(s)'.format(match)
         else :
             return 'No match'
 
     def solved(self):
+        """ Getter for _solved
+
+        Returns:
+            - _solved : (bool) True if secret code is broken
+        """
         return self._solved
 
 
