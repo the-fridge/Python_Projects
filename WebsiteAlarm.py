457768b9d2e707b0e68b184d831ba78fc5b7be90
pBouillon <pierre.bouillon@openmailbox.org>
2017-10-03 18:51:58 +0200

    reworked and commented website alarm

--- WebsiteAlarm.py
+++ WebsiteAlarm.py
@@ -1,49 +1,56 @@
-# Author - Shivam Sharma 
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# authors: Shivam Sharma / Pierre Bouillon
 
-# We use Bookmarks to remember the important websites which we think we will use them very often in future.
-# Here’s a simple python code which takes the URL of the website as its first input and the time you want to open it as the second input.
+""" `WebsiteAlarm.py` asks for a website and an alarm to be
+set, then open the browser when time reach the alarm
+"""
 
-# As the time reaches your given value of the time, it’ll open the URL that you requested in the web browser automatically.
-# In this code, we’ll import two python modules – Time and Webbrowser.
-
-# Import the time module, it provides various time-related
-# functions.
 import time
- 
-# Import the webbrowser module, it is used to
-# display various HTML documents to the user.
+from time import sleep
+
 import webbrowser
- 
-# First Input: It is the time of the form
-# 'Hours:Minutes:Seconds' that you'll
-# use to set the alarm.
-Set_Alarm = raw_input("Set the website alarm as H:M:S:")
- 
-# Second Input: It is the URL that you want
-# to open on the given time.
-url = raw_input("Enter the website you want to open:")
+from webbrowser import open
 
-# check if the url has http
-condition = "http" or "https" not in url 
+class BrowserAlarm(object):
+    """ Check an alarm every .5 seconds
+    Open a web browser when ringing
+    
+    Attributes:
+        - _alarm : (int) time provided by user
+        - _url   : (int) url to open on ringing
+    """
+    def __init__(self, url, alarm):
+        self._alarm = alarm
+        self._url   = url
+        self._check_param()
+        self._warn_launch()
 
-if condition == True:
-	url = "http://" + url
-# This is the actual time that we will use to print.
-Actual_Time = time.strftime("%I:%M:%S")
- 
-# This is the while loop that'll print the time
-# until it's value will be equal to the alarm time
-while (Actual_Time != Set_Alarm):
-    print "The time is " + Actual_Time
-    Actual_Time = time.strftime("%I:%M:%S")
-    time.sleep(1)
- 
- 
-# This if statement plays the role when its the
-# alarm time and executes the code within it.
-if (Actual_Time == Set_Alarm):
-    print "You should see your webpage now :-)"
- 
-    # We are calling the open()
-    # function from the webrowser module.
-    webbrowser.open(url)
+    def _check_param(self):
+        """ Check if the URL is correct
+        exit with an error message in the URL is incorrect
+        """
+        if 'http' not in self._url \
+            or 'https' not in self._url:
+            exit('Bad URL')
+
+    def _warn_launch(self):
+        """ Prints the alarm time
+        """
+        msg ='Now running until {}'
+        print (msg.format(self._alarm))
+
+    def loop(self):
+        """ Main loop
+        Check the time ever .5 second (to avoid skipping
+        a second)
+        Open the url on a webbrowser times are matching
+        """
+        while time.strftime("%I:%M:%S") != self._alarm :
+            sleep(.5)
+        print('Now launching browser ...')
+        open(self._url)
+
+if __name__ == '__main__':
+    alarm = BrowserAlarm('https://github.com', '06:48:00')
+    alarm.loop()
