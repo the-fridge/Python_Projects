480459aca0d0c76c121617f20d0662a87f4e79fb
pBouillon <pierre.bouillon@openmailbox.org>
2017-10-03 11:15:33 +0200

    ProjectX-xxx.py improvements

--- Project1-name.py
+++ Project1-name.py
@@ -2,49 +2,48 @@
 # -*- coding: utf-8 -*-
 # authors: Syed Marwan Jamal / Pierre Bouillon
 
-"""`Project1-name.py` is a program that asks for the user name
+"""`Project1-name.py` is a program that asks for the user's name
 Then it print the given name, and the lenght of this name.
 """
 
 def initial_way ():
-	""" asks the name, print it with its lenght
+    """ asks the name, print it with its lenght
 
-	Original code by Syed Marwan Jamal, using several `print()`
-	"""
-	print('Hello')
-	user_name = input('What is your name?\n')
-	print('It\'s nice to meet you', user_name)
-	print('Length of your name is:')
-	print(len(user_name))
+    Original code by Syed Marwan Jamal, using several `print()`
+    """
+    print('Hello')
+    user_name = input('What is your name?\n')
+    print('It\'s nice to meet you', user_name)
+    print('Length of your name is:')
+    print(len(user_name))
 
 def initial_way_2():
-	""" asks the name, print it with its lenght
+    """ asks the name, print it with its lenght
 
-	Original code by Syed Marwan Jamal, using a variable to
-	store the message and format it before display
-	"""
-	print('Hello')
-	user_name = input('What is your name?\n').
-	name_size = len(user_name)
-	msg = 'It\'s nice to meet you {0}!\nThe length of your name is: {1}'
-			.format(user_name, name_size)
-	print(msg)
+    Original code by Syed Marwan Jamal, using a variable to
+    store the message and format it before display
+    """
+    print('Hello')
+    user_name = input('What is your name?\n').
+    name_size = len(user_name)
+    msg = 'It\'s nice to meet you {0}!\nThe length of your name is: {1}'.format(user_name, name_size)
+    print(msg)
 
 def get_name_info():
-	""" asks the name, print it with its lenght
+    """ asks the name, print it with its lenght
 
-	Remake of initial_way() by Pierre Bouillon
-	"""
-	name = input('Hello,\nPlease, write your name: ')
-	print('---\nPlease to meet you {}. You name is {} char(s) long.\n---'
-		.format(name, len(name)))
+    Remake of initial_way() by Pierre Bouillon
+    """
+    name = input('Hello,\nPlease, write your name: ')
+    print('---\nPlease to meet you {}. You name is {} char(s) long.\n---'
+        .format(name, len(name)))
 
 
 def main() :
-	""" Main function of the script
+    """ Main function of the script
 
-	Call the other functions
-	"""
+    Call the other functions
+    """
     get_name_info()
     initial_way()
     initial_way_2()
--- Project2-age.py
+++ Project2-age.py
@@ -1,20 +1,57 @@
-# Author : Syed Marwan Jamal.
-# This program asks for user age,
-# returns their age in x years.
-# Please give your feedback on the program and way to improve it.
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# authors: Syed Marwan Jamal / Pierre Bouillon
 
+"""`Project1-name.py` is a program that asks for the user's age
+Then displays his age
+Then asks for a amount of years `years` and displays the user's 
+age when `years` years will be passed
 
-print('Hello')
-user_age = int(input('What is your age? \n'))
-print('Oh so your age is', user_age)
-years = int(input('Enter a number to know your age after that many years \n'))
-print('You will be',years+user_age, 'in', years ,'years')
+"""
 
+def initial_way ():
+    """ asks the age, print it and print it in a few years
 
-# Another way of doing
-user_age = int(input('Hello!\nWhat is your age? '))
-# Formatting will make the message looks better
-response = 'Oh so your age is {0}.'.format(user_age)
-print(response)
-years = int(input('Enter a number to know your age after that many years: '))
-print('You will be', years + user_age, 'in', years, 'years.')
+    Original code by Syed Marwan Jamal, using several `print()`
+    """
+    print('Hello')
+    user_age = int(input('What is your age? \n'))
+    print('Oh so your age is', user_age)
+    years = int(input('Enter a number to know your age after that many years \n'))
+    print('You will be',years+user_age, 'in', years ,'years')
+
+def initial_way_2():
+    """ asks the age, print it and print it in a few years
+
+    Original code by Syed Marwan Jamal, using a variable to
+    store the message and format it before display
+    """
+    user_age = int(input('Hello!\nWhat is your age? '))
+    response = 'Oh so your age is {0}.'.format(user_age)
+    print(response)
+    years = int(input('Enter a number to know your age after that many years: '))
+    print('You will be', years + user_age, 'in', years, 'years.')
+
+def get_age_info():
+    """ asks the age, print it and print it in a few years
+
+    Remake of initial_way() by Pierre Bouillon
+    """
+    age   = input('Hello,\nPlease, write your age: ')
+    years = input('Input a number to see your age in the future: ')
+    print('---\nYour age is {} and you will be {} in {} years.\n---'
+        .format(age, 
+            int(age) + int(years), 
+            years)
+        )
+
+def main() :
+    """ Main function of the script
+
+    Call the other functions
+    """
+    get_age_info()
+
+if __name__ == '__main__':
+    main()
+
