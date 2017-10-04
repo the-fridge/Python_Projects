e6949cc576dba84a20f3355b47b21b3315613090
Pranav Goel <pranavgoel_25@hotmail.com>
2017-10-04 13:39:03 +0530

    Comment base64meme.py

--- base64meme.py
+++ base64meme.py
@@ -1,5 +1,9 @@
+"""
+This program prints the base64 encoding of a string and then prints the base64 encoding of the obtained encoding and so on until the program is stopped by the user.
+"""
+
 import base64
 x = 'hacktoberfest'
-while True:
-    x = base64.b64encode(str(x).encode('ascii'))
+while True:  # Runs the program until stopped by the user
+    x = base64.b64encode(str(x).encode('ascii'))  # Encodes the string in base64
     print(x)
