92bbd6d6f1e8483483fe10e0cb2fceafc49233fe
Jonas H <33072340+the-fridge@users.noreply.github.com>
2017-10-25 00:36:06 +0200

    my take on it

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,11 +3,11 @@
 """
 This program is the implementation of the famous fizzbuzz program.
 If the number is divisible by both 3 and 5, then the progrma prints 'FizzBuzz'.
-If the enumber is only divisible by 3, then the program prints 'Fizz'
-If the enumber is only divisible by 5, then the program prints 'Buzz'
+If the number is only divisible by 3, then the program prints 'Fizz'.
+If the number is only divisible by 5, then the program prints 'Buzz'.
+If the number is not divisible by 3 nor by 5 the programm prints the number.
 """
 def fizz_buzz(num):
-    print(num)
     if num % 15 == 0:
         print("FizzBuzz")
     elif num % 5 == 0:
@@ -15,7 +15,7 @@ def fizz_buzz(num):
     elif num % 3 == 0:
         print("Fizz")
     else:
-        print("Number is not divisible by 3 or 5 or 15")
+        print(num)
 
 for i in range(1, 101):
     fizz_buzz(i)
